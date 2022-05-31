fcc4c9419c56025455664d05b05fd137bd1e3791
Shourov98hassan <kamruzzamanshourov98@gmail.com>
2022-05-31 00:15:38 -0700

    create navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import logo3 from '../../images/logo3.png'
+import './Header.css'
 
 const Header = () => {
     return (
-        <div>
+        <nav className='header'>
+            <img src={logo3} alt="" />
+            <div>
+                <a href="/home">Home</a>
+                <a href="/food items">Food Items</a>
+                <a href="/about">About</a>
+                <a href="/contact">Contact</a>
+            </div>
+
             
-        </div>
+        </nav>
     );
 };
 
